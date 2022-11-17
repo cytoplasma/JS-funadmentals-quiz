3e367897a66fc26e0617c1e72ea7875861bee9df
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-11-16 18:11:54 -0800

    set up variables in script.js for DOM references, and set up the script.js comments as a blueprint

---
+++ assets/js/scores.js
@@ -0,0 +1,5 @@
+function printHighscores() {
+// need to work on script.js before retrieving from a local storage
+    var highscores = JSON.parse()
+
+}
---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//variables mad efor DOM references!
+var questionEl = document.getElementById('questions')
+var timerEl = document.getElementById('timer')
+var choicesEl = document.getElementById('choices')
+var submitBtn = document.getElementById('submit')
+var startBtn = document.getElementById('start')
+
+
+//sort out functions that are needed for simplicity
+
+//this funciton's purpose is to change the css property
+//from hidden to showing the questions and the hiding the start
+function startQuiz() {
+    //start by hiding the start screen
+    //reveal the questions by changing 'display:none'
+}
+
+//function's purpose: 
+function showQuestion() {
+    //todo: i want to reveal questions that are stored in
+    //an array inside of a separate js file
+}
+
+//function's purpose:
+function questionAnswer() {
+
+}
+
+//function's purpose:
+function quizEnd() {
+
+}
+
+//function's purpose:
+function saveScore() {
+
+}
+
+//function's purpose:
+function timerTick() {
+
+}
+
+//need to incorporate interactive methods like .onclick (need to look more into it)
